7892fabe98d2c8370ff2c6f034e80410d417678d
Codename26 <slava.burchenko@gmail.com>
2017-10-21 18:54:00 +0300

    v1.0001

--- app/src/main/java/com/codename26/geofenceapplication/MainActivity.java
+++ app/src/main/java/com/codename26/geofenceapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.codename26.geofenceapplication;
 
+import android.Manifest;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION }, 1);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
     }
 }
